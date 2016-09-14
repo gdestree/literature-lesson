18c18db78bf127058d61ff386da9078c088f79bb
Gregor Destrée <gregordestree@gmail.com>
2016-09-14 17:24:03 +0200

    Add autotag method

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,9 +1,21 @@
 class Author < ActiveRecord::Base
-	
+
+	after_find :auto_tagger
+
 	has_many :poems
 	has_many :short_stories
 	has_many :taggings, as: :taggable
 	has_many :tags, through: :taggings
 
 	validates :name, :biography, presence: true
+
+	def auto_tagger
+		# first delete current taggings
+		taggings.delete_all
+		# save the content into an array content type is biography for author and content for poems/stories
+		array_of_content_words = biography.downcase.split(" ")
+		# Iterate over all tags and if the array contains one of the tags add that tag to the instances tags
+		Tag.all.each {|tag| tags << tag if array_of_content_words.include?(tag.name)}
+	end
+
 end
